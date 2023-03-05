6020b1d79f557005be354b630aaeb41089e328e9
rangapin <rangapin@gmx.com>
2023-03-05 05:52:38 +0100

    added validation to sign up

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,10 +1,43 @@
 <?php 
 
 if (isset($_POST["submit"])) {
-    echo "It works";
+
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $username = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+    $pwdRepeat = $_POST["pwdrepeat"];
+
+require_once "dbh.inc.php";
+require_once "functions.inc.php";
+
+if (emptyInputSignup($name, $email, $username, $pwd, $pwdrepeat) !== false) {
+    header("location: ../signup.php?error=emptyinput");
+    exit();
 }
+if (invalidUid($username) !== false) {
+        header("location: ../signup.php?error=invaliduid");
+        exit();
+}
+if (invalidEmail($email) !== false) {
+    header("location: ../signup.php?error=invalidemail");
+    exit();
+}
+if (pwdMatch($pwd, $pwdrepeat) !== false) {
+    header("location: ../signup.php?error=passworddonotmatch");
+    exit();
+}
+if (uidExists($conn, $username, $email) !== false) {
+    header("location: ../signup.php?error=usernametaken");
+    exit();
+}
+
+createUser($name, $email, $username, $pwd);
+}
+
 else {
     header("location: ../signup.php");
+    exit();
 }
 
 ?>
